8da1e7592124f44e1fd9706fbad6070227271bf3
Afton Lawver <afton.terminalai@gmail.com>
2021-10-14 15:16:36 -0500

    Added a file that uses the pyperclip module (copies text from command prompt).

--- pw.py
+++ pw.py
@@ -1,3 +1,20 @@
 #! python3
 # pw.py - An insecure password locker program.
 
+import sys
+import pyperclip
+
+PASSWORDS = {'email': 'Azula1002',
+             'gmail': 'MyPassword21'}
+
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for {} copied to clipboard'.format(account))
+else:
+    print('There is no account named {}'.format(account))
